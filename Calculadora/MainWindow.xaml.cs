9e97436b5d9a1bfc8dc7da528080a6a007cf54c7
alejandro <chipanaalejandro4@gmail.com>
2023-08-13 18:08:31 -0400

    ya funciona el cs

--- Calculadora/MainWindow.xaml.cs
+++ Calculadora/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace Calculadora
     /// </summary>
     public partial class MainWindow : Window
     {
+        double Numero1 = 0, Numero2 = 0;
+        char Operador;
         public MainWindow()
         {
             InitializeComponent();
@@ -43,14 +45,117 @@ namespace Calculadora
             Application.Current.Shutdown();
         }
 
+        private void VaciarVariable(object sender, RoutedEventArgs e)
+        {
+            txtResultado.Text = "0";
+        }
+
         private void btnBorrarTodo_Click(object sender, RoutedEventArgs e)
         {
-
+            Numero1 = 0;
+            Numero2= 0;
+            Operador = '\0';
+            txtResultado.Text = "0";
         }
 
-        private void btnBorrar_Click(object sender, RoutedEventArgs e)
+        private void btnBorrarUno_Click(object sender, RoutedEventArgs e)
         {
+            if (txtResultado.Text.Length > 1 )
+            {
+                txtResultado.Text = txtResultado.Text.Substring(0,txtResultado.Text.Length - 1);
+            }
+            else
+            {
+                txtResultado.Text= "0"; 
+            }
+        }
+        private void btnInvertir_Click(object sender, RoutedEventArgs e)
+        {
+            Numero1=double.Parse(txtResultado.Text);
+            Numero1 *= -1;
+            txtResultado.Text = Numero1.ToString();
+        }
+        private void AgregarNumero_Click(object sender, RoutedEventArgs e)
+        {
+            //se llama el boton seleccionado en la variable var
+            var boton = ((Button)sender);
+            //se verifica se el texbox est vacio o no
+            if (txtResultado.Text == "0")
+            {
+                txtResultado.Text = "";
+            }
+            //se agrega el numero
+            txtResultado.Text += boton.Content;
+        }
+
+        private void btnPunto_Click(object sender, RoutedEventArgs e)
+        {
+            if (!txtResultado.Text.Contains("."))
+            {
+                txtResultado.Text += ".";
+            }
+        }
+
+        private void Operador_Click(object sender, RoutedEventArgs e)
+        {
+            var boton = ((Button)sender);
+
+            //asignamos el contenido del texbox a la primero variable
+            Numero1 = double.Parse(txtResultado.Text);
+
+            //se asigna el tag de cada boton a la variable operador
+            Operador = Convert.ToChar(boton.Tag);
+            //se verifica el tipo de operacion en caso de no 
+            //necesitar otra varible aparece el resultado direccto 
+            if(Operador == '²')
+            {
+                Numero1 = Math.Pow(Numero1,2);
+                txtResultado.Text = Numero1.ToString();
+            }else if(Operador == '√')
+            {
+                Numero1 = Math.Sqrt(Numero1);
+                txtResultado.Text = Numero1.ToString();
+            }
+            else
+            {
+                txtResultado.Text = "0";
+            }
+        }
+
+
+        private void btnIgual_Click(object sender, RoutedEventArgs e)
+        {
+            Numero2 = double.Parse(txtResultado.Text);
+
+            switch (Operador)
+            {
+                case '+':
+
+                    txtResultado.Text = (Numero1 + Numero2).ToString();
+                    Numero1 = Convert.ToDouble(txtResultado.Text);
+                    break;
+                case '-':
+                    txtResultado.Text = (Numero1 - Numero2).ToString();
+                    Numero1 = Convert.ToDouble(txtResultado.Text);
+                    break;
+                case 'x':
+                    txtResultado.Text = (Numero1 * Numero2).ToString();
+                    Numero1 = Convert.ToDouble(txtResultado.Text);
+                    break;
+                case '/':
+                    if (txtResultado.Text != "0")
+                    {
+                        txtResultado.Text = (Numero1 / Numero2).ToString();
+                        Numero1 = Convert.ToDouble(txtResultado.Text);
+                    }
+                    else
+                    {
+                        MessageBox.Show("No es posible la divison entre cero!!!!");
+                    }
+                    break;
+            }
 
         }
+
     }
 }
